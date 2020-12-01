b68bd7593519dc451da798d53be4ab1500859444
silvestre_perret_craft <silvestre.perret@craft.ai>
2020-12-01 10:28:24 +0100

    silvestre/day-01

---
+++ day-01/part-1/silvestre.jl
@@ -0,0 +1,23 @@
+function run(s)
+    # Your code here
+    entries = split(s, "\n")
+    entries = map(x -> tryparse(Int, x), entries)
+    for el1 in entries
+        for el2 in entries
+            if el1 + el2 == 2020
+                return el1 * el2 
+            end
+        end
+    end
+    return 0
+end
+
+#########################################
+
+function main()
+    res, time, memory = @timed run(ARGS[1])
+    println("_duration:$(time*1000)")
+    println(res)
+end
+
+main()
---
+++ day-01/part-2/silvestre.jl
@@ -0,0 +1,28 @@
+function run(s)
+    # Your code here    
+    entries = split(s, "\n")
+    entries = map(x -> tryparse(Int, x), entries)
+    for (idx1, el1) in enumerate(entries)
+        for (idx2, el2) in enumerate(entries[idx1+1:end])
+            if el1 + el2 > 2020
+                continue
+            end
+            for el3 in entries[idx1+idx2+1:end]
+                if el1 + el2 + el3 == 2020
+                    return el1 * el2 * el3
+                end
+            end
+        end
+    end
+    return 0
+end
+
+#########################################
+
+function main()
+    res, time, memory = @timed run(ARGS[1])
+    println("_duration:$(time*1000)")
+    println(res)
+end
+
+main()
