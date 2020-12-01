d112ac79dbd45c0586970d14a61f8133382b632a
Julien Raspaud <julien.raspaud@illuin.tech>
2020-12-01 19:55:39 +0100

    don't judge me

---
+++ day-01/part-1/djou.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func run(s string) interface{} {
+	var ts = strings.Split(s, "\n")
+	var l = len(ts)
+	ti := make([]int, l)
+	for i, t := range ts {
+		ti[i], _ = strconv.Atoi(t)
+	}
+	for i := 0; i < l; i++ {
+		for j := i + 1; j < l; j++ {
+			if ti[i] + ti[j] == 2020 {
+				return ti[i] * ti[j]
+			}
+		}
+	}
+	return 0
+}
+
+func main() {
+	// Uncomment this line to disable garbage collection
+	// debug.SetGCPercent(-1)
+
+	// Read input from stdin
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start resolution
+	start := time.Now()
+	result := run(string(input))
+
+	// Print result
+	fmt.Printf("_duration:%f\n", time.Now().Sub(start).Seconds()*1000)
+	fmt.Println(result)
+}
