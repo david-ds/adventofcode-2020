e6ba6927aa19f577e7ad69ecd856c405ce18b131
David Dos Santos <david-ds@users.noreply.github.com>
2020-12-01 08:36:48 -0500

    [day-01] handle special case with 1010

--- day-01/part-1/david.py
+++ day-01/part-1/david.py
@@ -1,5 +1,6 @@
 from tool.runners.python import SubmissionPy
 
+from collections import defaultdict
 
 class DavidSubmission(SubmissionPy):
 
@@ -7,8 +8,17 @@ class DavidSubmission(SubmissionPy):
         # :param s: input in string format
         # :return: solution flag
         # Your code goes here
-        entries = {int(x) for x in s.split("\n")}
-        for i in entries:
+        counter = defaultdict(int)
+        for x in s.split("\n"):
+            counter[int(x)] += 1
+
+        # special case for 1010
+        if counter[1010] >= 2 :
+            return 1010*1010
+
+        del counter[1010]
+
+        for i in counter.keys():
             j = 2020-i
-            if j in entries:
+            if j in counter.keys():
                 return i*j
