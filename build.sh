06d2686da406c375c69b70576bcd7d8ac291838c
Ayc0 <ayc0.benj@gmail.com>
2020-12-02 10:45:56 +0100

    change build.sh to run on last commit change on master

--- build.sh
+++ build.sh
@@ -6,4 +6,15 @@ set -ev
 export PATH=$PATH:~/.cargo/bin:$GOROOT/bin
 export PYENV_VERSION=3.7
 
-git --no-pager diff --name-only origin/master -- | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
+run() {
+    echo $1 | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
+}
+
+if [ "$(git branch --show-current)" = "master" ];
+then
+    run "$(git --no-pager diff --name-only HEAD HEAD^ --)"
+else
+    run "$(git --no-pager diff --name-only origin/master --)"
+fi
+
+
