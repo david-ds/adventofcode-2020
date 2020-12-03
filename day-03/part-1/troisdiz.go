1b79313b8a304e4aceaa0c62af7aabe8e5848787
Denis Rampnoux <denis.rampnoux@datadoghq.com>
2020-12-03 13:26:24 +0100

    Denis day 03 part 1 ok

--- day-03/part-1/troisdiz.go
+++ day-03/part-1/troisdiz.go
@@ -4,11 +4,57 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+    "strings"
 	"time"
 )
 
+func parseData(input string) [][]bool {
+    var result [][]bool
+    for _, line := range strings.Split(input, "\n") {
+        symbols := make([]bool, len(line))
+        for idx, letter := range line {
+            symbols[idx] = (letter == '#')
+        }
+        result = append(result, symbols)
+    }
+    /*
+    for _, line := range result {
+        for _, cell := range line {
+            if cell {
+                fmt.Print("#")
+            } else {
+                fmt.Print(".")
+            }
+        }
+        fmt.Print("\n")
+    }
+    */
+    return result
+}
+
+func puzzle(input [][]bool, right int, down int) int {
+    height := len(input)
+    width := len(input[0])
+
+    fmt.Printf("Height : %d, width : %d\n", height, width)
+
+    var currentHeight, currentRight int
+    var treeCount int
+    for currentHeight <= height {
+        currentHeight += down
+        currentRight = (currentRight + right) % width
+
+        if currentHeight < height && input[currentHeight][currentRight] {
+            treeCount += 1
+        }
+    }
+    return treeCount
+}
+
 func run(s string) interface{} {
 	// Your code goes here
+    grid := parseData(s)
+    return puzzle(grid, 3, 1)
 }
 
 func main() {
