a98f08f1024652d85610a1f4f02cfd073077b3a6
AymericBebert <aymeric.bernard@hotmail.fr>
2020-12-03 09:52:55 +0100

    Bebert day 03 small opti

--- day-03/part-2/bebert.pyx
+++ day-03/part-2/bebert.pyx
@@ -1,14 +1,37 @@
+cimport cython
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing
+@cython.cdivision(True)     # Deactivate zero division check
 cpdef long run(s):
     trees = s.strip().splitlines()
-    h = len(trees)
-    w = len(trees[0])
+    cdef int w = len(trees[0])
+    cdef int h = len(trees)
 
-    result = 1
-    for slope in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]:
+    cdef int slopes_y[5]
+    slopes_y[0] = 1
+    slopes_y[1] = 1
+    slopes_y[2] = 1
+    slopes_y[3] = 1
+    slopes_y[4] = 2
+
+    cdef int slopes_x[5]
+    slopes_x[0] = 1
+    slopes_x[1] = 3
+    slopes_x[2] = 5
+    slopes_x[3] = 7
+    slopes_x[4] = 1
+
+    cdef long result = 1
+    cdef int count = 0
+
+    cdef int y
+    cdef int i
+    for i in range(5):
         count = 0
         for y in range(h):
-            if y % slope[0] == 0:
-                count += 1 if trees[y][((y // slope[0]) * slope[1]) % w] == "#" else 0
+            if y % slopes_y[i] == 0:
+                count += 1 if trees[y][((y // slopes_y[i]) * slopes_x[i]) % w] == "#" else 0
         result *= count
 
     return result
