0fc95c9e57e23f8ee492259149400d2abc06b787
Ayoub Sbai <ayoub.sbai@datadoghq.com>
2020-12-03 12:16:13 +0100

    [ayoub] add day-03

--- day-03/part-2/ayoub.cpp
+++ day-03/part-2/ayoub.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <string>
 #include <ctime>
 
 using namespace std;
 
-const uint64_t SLOPE_RIGHT[5] = {1, 3, 5, 7, 1};
-const uint64_t SLOPE_DOWN[5] =  {1, 1, 1, 1, 2};
+const long SLOPE_RIGHT[5] = {1, 3, 5, 7, 1};
+const long SLOPE_DOWN[5] =  {1, 1, 1, 1, 2};
 
-const uint64_t WIDTH = 31;
-const uint64_t HEIGHT = 323;
+const long WIDTH = 31;
+const long HEIGHT = 323;
 
-uint64_t run(char* s) {
-    uint64_t res = 1;
-    for (uint64_t k = 0; k < 5; k++) {
-        uint64_t j = -SLOPE_RIGHT[k], count = 0;
-        for (uint64_t i = 0; i < HEIGHT; i += SLOPE_DOWN[k]) {
+long run(char* s) {
+    long res = 1;
+    for (long k = 0; k < 5; k++) {
+        long j = -SLOPE_RIGHT[k], count = 0;
+        for (long i = 0; i < HEIGHT; i += SLOPE_DOWN[k]) {
             j = (j + SLOPE_RIGHT[k]) % WIDTH;
             if (s[i*(WIDTH+1)+j] == '#') count++;
         }
@@ -30,7 +29,7 @@ int main(int argc, char** argv) {
     }
 
     clock_t start = clock();
-    uint64_t answer = run(argv[1]);
+    long answer = run(argv[1]);
     
     cout << "_duration:" << float( clock () - start ) * 1000.0 /  CLOCKS_PER_SEC << "\n";
     cout << answer << "\n";
