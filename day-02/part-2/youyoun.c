a1a298c3d2272bcaf52e79a3e73521c331ceedbe
Younes Belkouchi <belkouchiyounes@gmail.com>
2020-12-04 14:55:32 +0100

    youyoun-day-02-C

---
+++ day-02/part-2/youyoun.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int run(char* s) {
+  char* inp = s;
+  int good_pwd_counter = 0;
+
+  char special;
+  int special_counter = 0;
+  int min = 0;
+  int max = 0;
+  int i = 1;
+  int j = 0;
+
+  while (j < 1000) {
+    min = 0;max = 0; special_counter = 0;
+
+    min = atoi(inp);
+    while (*inp != '-') {
+        inp++;
+    }
+    inp++;
+
+    max = atoi(inp);
+    while (*inp != ' ') {
+        inp++;
+    }
+    inp++;
+
+    special = *inp;
+    inp++; inp++; inp++;
+
+    i = 1;
+    while ((*inp != '\n') & (*inp != '\0')) {
+        if ((i == min) || (i == max)) {
+            if (*inp == special) {
+                special_counter += 1;
+            }
+        }
+        inp++; i++;
+    }
+
+    if (special_counter == 1) {
+        good_pwd_counter++;
+    }
+    j++;
+  }
+  return good_pwd_counter;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        printf("Missing one argument\n");
+        exit(1);
+    }
+
+    clock_t start = clock();
+    int answer = run(argv[1]);
+
+    printf("_duration:%f\n%i\n", (float)( clock () - start ) * 1000.0 /  CLOCKS_PER_SEC, answer);
+    return 0;
+}
