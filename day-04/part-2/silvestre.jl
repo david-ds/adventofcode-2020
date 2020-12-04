b3a82991ea894a7b74ab22781bae43f602b493d5
silvestre_perret_craft <silvestre.perret@craft.ai>
2020-12-04 14:37:45 +0100

    test

--- day-04/part-2/silvestre.jl
+++ day-04/part-2/silvestre.jl
@@ -1,8 +1,8 @@
-function is_between(value, low, high)
+function is_between(value::String, low::Int, high::Int)::Bool
     return low <= parse(Int, value) <= high
 end
 
-function is_valid_height(value)
+function is_valid_height(value::String)::Bool
     if endswith(value, "cm")
         return is_between(value[1:end - 2], 150, 193)
     elseif endswith(value, "in")
@@ -13,38 +13,35 @@ function is_valid_height(value)
 end
 
 
-function check_constraints(fields)
-    for k in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-        if !(k in keys(fields))
-            return false
-        end
-        value = fields[k]
-        if k == "byr" && !is_between(value, 1920, 2002)
-            return false
-        elseif k == "iyr" && !is_between(value, 2010, 2020)
-            return false
-        elseif k == "eyr" && !is_between(value, 2020, 2030)
-            return false
-        elseif k == "hgt" && !is_valid_height(value)
-            return false
-        elseif k == "hcl" && !occursin(r"^#[0-9a-f]{6}$", value)
-            return false
-        elseif k == "ecl" && !(value in Set(("amb", "blu", "brn", "gry", "grn", "hzl", "oth")))
-            return false
-        elseif k == "pid" && !occursin(r"^[0-9]{9}$", value)
-            return false
-        end
+function check_constraints(fields::Dict{String,String})::Bool
+    required_fields = Set{String}(("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"))
+    if !(issubset(required_fields, keys(fields)))
+        return false
+    elseif !is_between(fields["byr"], 1920, 2002)
+        return false
+    elseif !is_between(fields["iyr"], 2010, 2020)
+        return false
+    elseif !is_between(fields["eyr"], 2020, 2030)
+        return false
+    elseif !is_valid_height(fields["hgt"])
+        return false
+    elseif !occursin(r"^#[0-9a-f]{6}$", fields["hcl"])
+        return false
+    elseif !(fields["ecl"] in Set{String}(("amb", "blu", "brn", "gry", "grn", "hzl", "oth")))
+        return false
+    elseif !occursin(r"^[0-9]{9}$", fields["pid"])
+        return false
     end
     return true
 end
 
-function run(s)
+function run(s::String)::Int
     # Your code here
-    passports = split(s, "\n\n")
-    counter = 0
+    passports::Array{String} = split(s, "\n\n")
+    counter::Int = 0
     for passport_str in passports
-        fields = split(replace(passport_str, '\n' => ' '), ' ')
-        fields = Dict(map(x -> split(x, ":"), fields))
+        tags::Array{String} = split(replace(passport_str, '\n' => ' '), ' ')
+        fields = Dict{String,String}(map(x -> split(x, ":"), tags))
         if check_constraints(fields)
             counter += 1
         end
