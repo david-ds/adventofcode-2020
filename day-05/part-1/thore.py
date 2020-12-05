438d3a4ead2646d20140439ca152e69b7e9e6104
Thomas Renault <thomas.renault56@gmail.com>
2020-12-05 18:23:24 +0100

    [thore] simplify day 5

--- day-05/part-1/thore.py
+++ day-05/part-1/thore.py
@@ -12,12 +12,7 @@ class ThoreSubmission(SubmissionPy):
 
     def get_seat_id(self, bsp):
         binary_string = "".join(["1" if c in {"B", "R"} else "0" for c in bsp.strip()])
-        n = int(binary_string, 2)
-
-        row = n >> 3
-        col = n & 0b111
-
-        return row * 8 + col
+        return int(binary_string, 2)
 
 
 def test_day5():
