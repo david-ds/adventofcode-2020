3fa358d468409d53df654c4404086c9f313bdc2f
Antoine Salon <antoine.salon@hotmail.com>
2020-12-05 16:07:40 -0800

    [kimsit] Day 5 update

--- day-05/part-1/evqna.py
+++ day-05/part-1/evqna.py
@@ -4,10 +4,8 @@ from tool.runners.python import SubmissionPy
 class EvqnaSubmission(SubmissionPy):
 
     def decode_seat(self, encoding):
-        binary_row = [{'F': '0', 'B': '1'}[c] for c in encoding[:7]]
-        binary_col = [{'L': '0', 'R': '1'}[c] for c in encoding[7:]]
-        row, col = int(''.join(binary_row), 2), int(''.join(binary_col), 2)
-        return 8 * row + col
+        binary_repr = encoding.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+        return int(binary_repr, 2)
 
     def run(self, s):
         return max([self.decode_seat(e) for e in s.splitlines()])
