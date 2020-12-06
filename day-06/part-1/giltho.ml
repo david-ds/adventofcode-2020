c66a919cccd210bf5c9242bbbf3bd8a40efee298
Sacha Ayoun <sachaayoun@gmail.com>
2020-12-06 11:16:54 +0000

    slight optim

--- day-06/part-1/giltho.ml
+++ day-06/part-1/giltho.ml
@@ -6,25 +6,38 @@ let add_to_array arr c =
   let ofs = Char.code c - char_code_a in
   arr.(ofs) <- true
 
+let count_true_and_reset arr =
+  let acc = ref 0 in
+  for i = 0 to 25 do
+    if arr.(i) then (
+      incr acc;
+      arr.(i) <- false )
+  done;
+  !acc
+
 let count_true arr =
-  Array.fold_left (fun acc x -> if x then acc + 1 else acc) 0 arr
+  let acc = ref 0 in
+  for i = 0 to 25 do
+    if arr.(i) then incr acc
+  done;
+  !acc
 
 let readInput input =
   let len = String.length input in
-  let rec loop i total state current_hash =
-    if i >= len then total
+  let hash = Array.make 26 false in
+  let rec loop i total state =
+    if i >= len then total + count_true hash
     else
       match (state, input.[i]) with
       | NextPerson, '\n' ->
-          let total = total + count_true current_hash in
-          let arr = Array.make 26 false in
-          loop (i + 1) total NextGroup arr
-      | Reading, '\n' -> loop (i + 1) total NextPerson current_hash
+          let total = total + count_true_and_reset hash in
+          loop (i + 1) total NextGroup
+      | Reading, '\n' -> loop (i + 1) total NextPerson
       | _, c ->
-          add_to_array current_hash c;
-          loop (i + 1) total Reading current_hash
+          add_to_array hash c;
+          loop (i + 1) total Reading
   in
-  loop 0 0 NextPerson (Array.make 26 false)
+  loop 0 0 NextPerson
 
 let run input = readInput input
 
--- day-06/part-2/giltho.ml
+++ day-06/part-2/giltho.ml
@@ -6,26 +6,37 @@ let add_to_array arr c =
   let ofs = Char.code c - char_code_a in
   arr.(ofs) <- arr.(ofs) + 1
 
+let count_equal_and_reset arr n =
+  let acc = ref 0 in
+  for i = 0 to 25 do
+    if Int.equal arr.(i) n then incr acc;
+    arr.(i) <- 0
+  done;
+  !acc
+
 let count_equal arr n =
-  Array.fold_left (fun acc x -> if x = n then acc + 1 else acc) 0 arr
+  let acc = ref 0 in
+  for i = 0 to 25 do
+    if Int.equal arr.(i) n then incr acc
+  done;
+  !acc
 
 let readInput input =
   let len = String.length input in
-  let rec loop i total state nb_people current_hash =
-    if i >= len then total + count_equal current_hash (nb_people + 1)
+  let hash = Array.make 26 0 in
+  let rec loop i total state nb_people =
+    if i >= len then total + count_equal hash (nb_people + 1)
     else
       match (state, input.[i]) with
       | NextPerson, '\n' ->
-          let total = total + count_equal current_hash nb_people in
-          let arr = Array.make 26 0 in
-          loop (i + 1) total NextGroup 0 arr
-      | Reading, '\n' ->
-          loop (i + 1) total NextPerson (nb_people + 1) current_hash
+          let total = total + count_equal_and_reset hash nb_people in
+          loop (i + 1) total NextGroup 0
+      | Reading, '\n' -> loop (i + 1) total NextPerson (nb_people + 1)
       | _, c ->
-          add_to_array current_hash c;
-          loop (i + 1) total Reading nb_people current_hash
+          add_to_array hash c;
+          loop (i + 1) total Reading nb_people
   in
-  loop 0 0 NextPerson 0 (Array.make 26 0)
+  loop 0 0 NextPerson 0
 
 let run input = readInput input
 
