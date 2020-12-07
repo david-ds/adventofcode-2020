dd6b613107df216bd1888d69d0eb3843cab0b98f
Denis Rampnoux <denis.rampnoux@datadoghq.com>
2020-12-07 10:04:50 +0100

    Denis day 5 part 1 ok

--- day-05/part-1/troisdiz.go
+++ day-05/part-1/troisdiz.go
@@ -4,11 +4,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+    "strings"
 	"time"
 )
 
+func decodeWithLetter(inputStr string, lowLetter string, upLetter string, nbOfItems int) int {
+
+    lowerBound := 0
+    upperBound := nbOfItems -1
+    for _, letter := range inputStr {
+        midLimit := lowerBound + (upperBound + 1 - lowerBound) / 2
+        if string(letter) == lowLetter {
+            upperBound = midLimit -1
+        } else {
+            lowerBound = midLimit
+        }
+    }
+    return lowerBound
+}
+
+func decodeId(input string) int {
+    row := decodeWithLetter(input[0:7], "F", "B", 128)
+    col := decodeWithLetter(input[7:10], "L", "R", 8)
+    return row * 8 + col
+}
+
 func run(s string) interface{} {
-	// Your code goes here
+
+    maxSitId := 0
+    for _, sit := range strings.Split(s, "\n") {
+        sitId := decodeId(sit)
+        if sitId > maxSitId {
+            maxSitId = sitId
+        }
+    }
+    return maxSitId
 }
 
 func main() {
