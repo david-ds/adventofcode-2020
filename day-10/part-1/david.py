95fd4b635fc1df28620a10a7bf66c5890b0d150c
David Dos Santos <david-ds@users.noreply.github.com>
2020-12-10 00:23:28 -0500

    add day-10

---
+++ day-10/part-1/david.py
@@ -0,0 +1,23 @@
+from tool.runners.python import SubmissionPy
+
+from collections import defaultdict
+
+class DavidSubmission(SubmissionPy):
+
+    def run(self, s):
+        """
+        :param s: input in string format
+        :return: solution flag
+        """
+        # Your code goes here
+        l = sorted([int(x) for x in s.split("\n")])
+        l = [0] + l + [l[-1]+3]
+        diffs = defaultdict(int)
+        for i in range(len(l)-1):
+            # assert(l[i+1]-l[i] in {1,2,3})
+            diffs[l[i+1]-l[i]] += 1
+
+        return diffs[3] * diffs[1]
+            
+            
+
---
+++ day-10/part-2/david.py
@@ -0,0 +1,30 @@
+from tool.runners.python import SubmissionPy
+
+from collections import defaultdict
+
+class DavidSubmission(SubmissionPy):
+
+    def run(self, s):
+        """
+        :param s: input in string format
+        :return: solution flag
+        """
+        # Your code goes here
+        l = sorted([int(x) for x in s.split("\n")])
+        l = [l[0]] + l + [l[-1]+3]
+        n = len(l)
+        cache = [0] * n
+        cache[0] = 1
+        for i in range(1,n):
+            if i-1 >= 0 and l[i] - l[i-1] <= 3:
+                cache[i] += cache[i-1]
+            if i-2 >= 0 and l[i] - l[i-2] <= 3:
+                cache[i] += cache[i-2]
+            if i-3 >= 0 and l[i] - l[i-3] <= 3:
+                cache[i] += cache[i-3]
+
+        return cache[n-1]
+            
+            
+            
+
