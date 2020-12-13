3422059c00b5dea031d04ee0001d412ad59ce7c6
Ayoub Sbai <ayoub.sbai@datadoghq.com>
2020-12-13 13:20:18 +0100

    [ayoub] fix day-13 p2

--- day-13/part-2/ayoub.cpp
+++ day-13/part-2/ayoub.cpp
@@ -5,14 +5,13 @@
 
 using namespace std;
 
-  // To compute x^y under modulo m 
+// To compute x^y under modulo m 
 uint64_t power(uint64_t x, uint64_t y, uint64_t m) {
-    if (y == 0)
-        return 1; 
-    uint64_t p = power(x, y / 2, m) % m; 
-    p = (p * p) % m; 
+    if (y == 0) return 1;
+    uint64_t p = power(x, y / 2, m) % m;
+    p = (p * p) % m;
   
-    return (y % 2 == 0) ? p : (x * p) % m; 
+    return (y % 2 == 0) ? p : (x * p) % m;
 }
   
 // Function to find modular inverse of a under modulo m 
@@ -29,19 +28,16 @@ uint64_t modInverse(uint64_t a, uint64_t m) {
 //  x % num[k-2] = rem[k-1]
 // Assumption: Numbers in num[] are pairwise coprime
 // (gcd for every pair is 1)
-uint64_t findMinX(uint64_t num[], uint64_t rem[], int k)
-{
+uint64_t findMinX(uint64_t num[], uint64_t rem[], int k) {
     // Compute product of all numbers
     uint64_t prod = 1;
-    for (int i = 0; i < k; i++)
-        prod *= num[i];
+    for (int i = 0; i < k; i++) prod *= num[i];
 
     // Initialize result
     uint64_t result = 0;
 
     // Apply above formula
-    for (int i = 0; i < k; i++)
-    {
+    for (int i = 0; i < k; i++) {
         uint64_t pp = prod / num[i];
         result += rem[i] * modInverse(pp, num[i]) * pp;
     }
