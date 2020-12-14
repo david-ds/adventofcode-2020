e3a052f8371bdedacdabdf1f324825ba40b325ef
silvestre_perret_craft <silvestre.perret@craft.ai>
2020-12-14 18:42:31 +0100

    opti parsing

--- day-02/part-1/silvestre.jl
+++ day-02/part-1/silvestre.jl
@@ -1,12 +1,25 @@
-function parse_line(line)
-    rule, password = split(line, ": ")
-    counts, char = split(rule, " ")
-    char = char[1] # convert to Char
-    lower_bound, upper_bound = map(x -> parse(Int, x), split(counts, "-"))
+function parse_line(line::String)::Tuple{Int,Int,Char,SubString}
+    lower_bound::Int = 1
+    upper_bound::Int = 1
+    char::Char = 'a'
+    
+    first_idx::Int = 1
+    for (idx, c) in enumerate(line)
+        if c == '-'
+            lower_bound = parse(Int, SubString(line, first_idx, idx - 1))
+            first_idx = idx + 1
+        elseif c == ' '
+            upper_bound = parse(Int, SubString(line, first_idx, idx - 1))
+            char = line[idx + 1]
+            first_idx = idx + 4
+            break
+        end    
+    end
+    password = SubString(line, first_idx, lastindex(line))
     return lower_bound, upper_bound, char, password
 end
 
-function count_char(char, string)
+function count_char(char::Char, string::SubString)::Int
     counter = 0
     for c in string
         if c == char
@@ -18,10 +31,9 @@ end
 
 function run(s)
     # Your code here
-    entries = split(s, "\n")
     count = 0
-    for line in entries
-        lower_bound, upper_bound, char, password = parse_line(line)
+    for line in readlines(IOBuffer(s))
+        (lower_bound, upper_bound, char, password) = parse_line(line)
         if lower_bound <= count_char(char, password) <= upper_bound
             count += 1
         end
--- day-02/part-2/silvestre.jl
+++ day-02/part-2/silvestre.jl
@@ -1,16 +1,29 @@
-function parse_line(line)
-    rule, password = split(line, ": ")
-    counts, char = split(rule, " ")
-    char = char[1] # convert to Char
-    pos1, pos2 = map(x -> parse(Int, x), split(counts, "-"))
-    return pos1, pos2, char, password
+function parse_line(line::String)::Tuple{Int,Int,Char,SubString}
+    lower_bound::Int = 1
+    upper_bound::Int = 1
+    char::Char = 'a'
+    
+    first_idx::Int = 1
+    for (idx, c) in enumerate(line)
+        if c == '-'
+            lower_bound = parse(Int, SubString(line, first_idx, idx - 1))
+            first_idx = idx + 1
+        elseif c == ' '
+            upper_bound = parse(Int, SubString(line, first_idx, idx - 1))
+            char = line[idx + 1]
+            first_idx = idx + 4
+            break
+        end    
+    end
+    password = SubString(line, first_idx, lastindex(line))
+    return lower_bound, upper_bound, char, password
 end
+
 
 function run(s)
     # Your code here
-    entries = split(s, "\n")
     count = 0
-    for line in entries
+    for line in readlines(IOBuffer(s))
         pos1, pos2, char, password = parse_line(line)
         if xor(password[pos1] == char, password[pos2] == char)
             count += 1
