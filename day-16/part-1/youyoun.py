6bfffbc5d5609315f7c9cf42dcad09cf40aa44c5
Youyoun <belkouchiyounes@gmail.com>
2020-12-16 15:37:11 +0100

    Slightly optimised part 1

--- day-16/part-1/youyoun.py
+++ day-16/part-1/youyoun.py
@@ -10,22 +10,22 @@ class YouyounSubmission(SubmissionPy):
         """
         s = s.splitlines()
         possible_vals = set()
-        i = 0
-        while True:
-            if s[i] != '':
-                _, range_ = s[i].split(':')
-                for x in range_.split(' or '):
-                    low, high = map(int, x.split('-'))
-                    possible_vals = possible_vals.union(set(range(low, high + 1)))
-                i += 1
-            else:
-                break
 
-        nearby_tickets = [list(map(int, x.split(','))) for x in s[i + 5:]]
+        for i in range(20):
+            _, range_ = s[i].split(':')
+            for x in range_.split(' or '):
+                low, high = map(int, x.split('-'))
+                possible_vals.add((low, high))
 
         error_rate = 0
-        for ticket in nearby_tickets:
-            for e in ticket:
-                if e not in possible_vals:
-                    error_rate += e
+        for ticket in s[25:]:
+            for e in map(int, ticket.split(',')):
+                is_valid = False
+                for r in possible_vals:
+                    if r[0] <= e <= r[1]:
+                        is_valid = True
+                        break
+                if is_valid:
+                    continue
+                error_rate += e
         return error_rate
