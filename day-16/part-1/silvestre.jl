7a6ea8c40bb9ab3afc086f6a91fe3e491fce43c4
silvestre_perret_craft <silvestre.perret@craft.ai>
2020-12-16 18:45:50 +0100

    silvestre/day-16

---
+++ day-16/part-1/silvestre.jl
@@ -0,0 +1,62 @@
+const RULES_MODE = 0
+const MY_TICKET_MODE = 1
+const NEARBY_TICKETS_MODE = 2
+
+
+function add_rule(line::String, line_idx::Int, rules::Array{Int,2})
+    first_idx = findnext(':', line, 1) + 1
+    second_idx = findnext('-', line, first_idx + 1) - 1
+    rules[2 * line_idx - 1, 1] = parse(Int, SubString(line, first_idx, second_idx))
+    first_idx = second_idx + 2
+    second_idx = findnext(' ', line, first_idx + 1) - 1
+    rules[2 * line_idx - 1, 2] = parse(Int, SubString(line, first_idx, second_idx))
+    first_idx = second_idx + 5
+    second_idx = findnext('-', line, first_idx + 1) - 1
+    rules[2 * line_idx, 1] = parse(Int, SubString(line, first_idx, second_idx))
+    first_idx = second_idx + 2
+    second_idx = lastindex(line)
+    rules[2 * line_idx, 2] = parse(Int, SubString(line, first_idx, second_idx))
+end
+
+function is_valid(value::Int, rules::Array, n_rules::Int)::Bool
+    return any(rules[i, 1] <= value <= rules[i, 2] for i in 1:n_rules)
+end
+
+function run(s)
+    rules = zeros(Int, (50, 2))
+    n_rules::Int = 0
+    
+    counter::Int = 0
+    mode::Int = RULES_MODE
+    for (idx, line) in enumerate(readlines(IOBuffer(s)))
+        if length(line) < 20
+            if length(line) == 0
+                mode += 1
+            end
+            continue
+        elseif mode == RULES_MODE
+            add_rule(line, idx, rules)
+            n_rules += 1
+        elseif mode == NEARBY_TICKETS_MODE
+            values = parse.(Int, split(line, ","))
+            for v in values
+                if !is_valid(v, rules, n_rules)
+                    counter += v
+                    break
+                end
+            end
+        end
+    end
+    return counter
+end
+
+#########################################
+
+function main()
+    run(ARGS[1])
+    res, time, memory = @timed run(ARGS[1])
+    println("_duration:$(time * 1000)")
+    println(res)
+end
+
+main()
