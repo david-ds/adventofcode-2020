77529822c706af6cbd16df9bfec80026a5347071
AymericBebert <aymeric.bernard@hotmail.fr>
2020-12-16 02:41:55 +0100

    Bebert day 15 (much warnings)

--- tool/runners/cython_aoc-setup.py
+++ tool/runners/cython_aoc-setup.py
@@ -7,10 +7,19 @@ from distutils.core import setup
 
 CYTHON_DEBUG = bool(os.getenv("CYTHON_DEBUG", ""))
 
+os.environ["CFLAGS"] = os.environ.get("CFLAGS", "") + " -O3"
+
 build_dir = sys.argv.pop()
 script_name = sys.argv.pop()
 
-os.environ["CFLAGS"] = os.environ.get("CFLAGS", "") + " -O3"
+included = set()
+include_dirs = []
+with open(script_name, "r") as script:
+    for line in script.readlines():
+        if line.startswith("cimport numpy") and "numpy" not in included:
+            import numpy
+            include_dirs.append(numpy.get_include())
+            included.add("numpy")
 
 setup(
     ext_modules=cythonize(
@@ -19,5 +28,6 @@ setup(
         quiet=not CYTHON_DEBUG,
         annotate=CYTHON_DEBUG,
         compiler_directives={"language_level": 3},
-    )
+    ),
+    include_dirs=include_dirs,
 )
