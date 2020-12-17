92974e9bcf89e90e9839bce88009c0451c476d6a
Thomas Renault <thomas.renault56@gmail.com>
2020-12-17 14:00:20 +0100

    [thore] day 17 simpler

--- day-17/part-1/thore.py
+++ day-17/part-1/thore.py
@@ -1,106 +1,70 @@
 from collections import defaultdict
-import copy
 from itertools import product
 
 from tool.runners.python import SubmissionPy
 
-ACTIVE = "#"
-INACTIVE = "."
-CYCLES = 6
-
 
 class ThoreSubmission(SubmissionPy):
-    def run(self, s, debug=False):
+    def run(self, s):
         """
         :param s: input in string format
         :return: solution flag
         """
-        pocket_dim = PocketDimension()
-        pocket_dim.parse_input(s)
+        n_dims = 3
+        n_cycles = 6
+        debug = False
 
+        neighbors_delta = [
+            x for x in product((-1, 0, 1), repeat=n_dims) if x != (0,) * n_dims
+        ]
+
+        active = {
+            (x, y) + (0,) * (n_dims - 2)
+            for x, line in enumerate(s.splitlines())
+            for y, c in enumerate(line)
+            if c == "#"
+        }
         if debug:
             print("Before any cycle:\n")
-            print(pocket_dim, "\n")
+            print(pformat_pocket_dim(active), "\n")
 
-        for cycle in range(CYCLES):
-            pocket_dim.step()
+        for cycle in range(n_cycles):
+            n_neighbors = defaultdict(int)
+            for cube in active:
+                for delta in neighbors_delta:
+                    neighbor = tuple(sum(x) for x in zip(cube, delta))
+                    n_neighbors[neighbor] += 1
+
+            new_active = set()
+            for cube, n in n_neighbors.items():
+                if cube in active:
+                    if n in [2, 3]:
+                        new_active.add(cube)
+                elif n == 3:
+                    new_active.add(cube)
+            active = new_active
 
             if debug:
                 print(f"After {cycle+1} cycles:\n")
-                print(pocket_dim, "\n")
-        return pocket_dim.get_activated_count()
+                print(pformat_pocket_dim(active), "\n")
+        return len(active)
 
 
-class PocketDimension:
-    def __init__(self):
-        self.active = set()
-        self.n_neighbors = defaultdict(int)
-        self.candidates = set()
-
-    def parse_input(self, s):
-        lines = s.replace(ACTIVE, "1").replace(INACTIVE, "0").splitlines()
-        for x, line in enumerate(lines):
-            for y, c in enumerate(line):
-                if c == "1":
-                    self.activate(x, y, 0)
-
-    def activate(self, x, y, z):
-        self.active.add((x, y, z))
-        self.candidates.discard((x, y, z))
-        for neighbor in self.get_neighbours(x, y, z):
-            self.n_neighbors[neighbor] += 1
-            if self.n_neighbors[neighbor] == 3:
-                self.candidates.add(neighbor)
-            elif self.n_neighbors[neighbor] == 4:
-                self.candidates.discard(neighbor)
-
-    def deactivate(self, x, y, z):
-        self.active.discard((x, y, z))
-        for neighbor in self.get_neighbours(x, y, z):
-            self.n_neighbors[neighbor] -= 1
-            if self.n_neighbors[neighbor] == 3:
-                self.candidates.add(neighbor)
-            elif self.n_neighbors[neighbor] == 2:
-                self.candidates.discard(neighbor)
-
-    def step(self):
-        old_active = self.active.copy()
-        old_n_neighbors = self.n_neighbors.copy()
-        old_candidates = self.candidates.copy()
-
-        for cube in old_active:
-            if old_n_neighbors[cube] not in [2, 3]:
-                self.deactivate(*cube)
-        for cube in old_candidates - old_active:
-            self.activate(*cube)
-
-    def get_activated_count(self):
-        return len(self.active)
-
-    @staticmethod
-    def get_neighbours(x, y, z):
-        for dx, dy, dz in product((-1, 0, 1), repeat=3):
-            if (dx, dy, dz) != (0, 0, 0):
-                yield x + dx, y + dy, z + dz
-
-    def __str__(self):
-        x_coords, y_coords, z_coords = zip(*self.active)
-        x_range = (min(x_coords), max(x_coords) + 1)
-        y_range = (min(y_coords), max(y_coords) + 1)
-        z_range = (min(z_coords), max(z_coords) + 1)
-        lines = []
-        for z in range(*z_range):
-            lines.append(f"z={z}")
-            for x in range(*x_range):
-                lines.append(
-                    "".join(
-                        ACTIVE if (x, y, z) in self.active else INACTIVE
-                        for y in range(*y_range)
-                    )
+def pformat_pocket_dim(active):
+    coords_by_dim = zip(*active)
+    ranges = [(min(coords), max(coords) + 1) for coords in coords_by_dim]
+    lines = []
+    for coords in zip(*[range(*r) for r in ranges[2:]]):
+        lines.append(f"(X,X,{','.join([str(c) for c in coords])})")
+        for x in range(*ranges[0]):
+            lines.append(
+                "".join(
+                    "#" if (x, y, *coords) in active else "." for y in range(*ranges[1])
                 )
+            )
 
-            lines.append("")
-        return "\n".join(lines)
+        lines.append("")
+    return "\n".join(lines)
 
 
 def test_day17_part1():
--- day-17/part-2/thore.py
+++ day-17/part-2/thore.py
@@ -1,107 +1,70 @@
 from collections import defaultdict
-import copy
 from itertools import product
 
 from tool.runners.python import SubmissionPy
 
-ACTIVE = "#"
-INACTIVE = "."
-CYCLES = 6
-
 
 class ThoreSubmission(SubmissionPy):
-    def run(self, s, debug=False):
+    def run(self, s):
         """
         :param s: input in string format
         :return: solution flag
         """
-        pocket_dim = PocketDimension(n_dim=4)
-        pocket_dim.parse_input(s)
+        n_dims = 4
+        n_cycles = 6
+        debug = False
 
+        neighbors_delta = [
+            x for x in product((-1, 0, 1), repeat=n_dims) if x != (0,) * n_dims
+        ]
+
+        active = {
+            (x, y) + (0,) * (n_dims - 2)
+            for x, line in enumerate(s.splitlines())
+            for y, c in enumerate(line)
+            if c == "#"
+        }
         if debug:
             print("Before any cycle:\n")
-            print(pocket_dim, "\n")
+            print(pformat_pocket_dim(active), "\n")
 
-        for cycle in range(CYCLES):
-            pocket_dim.step()
+        for cycle in range(n_cycles):
+            n_neighbors = defaultdict(int)
+            for cube in active:
+                for delta in neighbors_delta:
+                    neighbor = tuple(sum(x) for x in zip(cube, delta))
+                    n_neighbors[neighbor] += 1
+
+            new_active = set()
+            for cube, n in n_neighbors.items():
+                if cube in active:
+                    if n in [2, 3]:
+                        new_active.add(cube)
+                elif n == 3:
+                    new_active.add(cube)
+            active = new_active
 
             if debug:
                 print(f"After {cycle+1} cycles:\n")
-                print(pocket_dim, "\n")
-        return pocket_dim.get_activated_count()
+                print(pformat_pocket_dim(active), "\n")
+        return len(active)
 
 
-class PocketDimension:
-    def __init__(self, n_dim=3):
-        self.active = set()
-        self.n_neighbors = defaultdict(int)
-        self.candidates = set()
-        self.n_dim = n_dim
-
-    def parse_input(self, s):
-        lines = s.replace(ACTIVE, "1").replace(INACTIVE, "0").splitlines()
-        for x, line in enumerate(lines):
-            for y, c in enumerate(line):
-                if c == "1":
-                    cube = (x, y) + (0,) * (self.n_dim - 2)
-                    self.activate(cube)
-
-    def activate(self, cube):
-        self.active.add((cube))
-        self.candidates.discard((cube))
-        for neighbor in self.get_neighbours(cube):
-            self.n_neighbors[neighbor] += 1
-            if self.n_neighbors[neighbor] == 3:
-                self.candidates.add(neighbor)
-            elif self.n_neighbors[neighbor] == 4:
-                self.candidates.discard(neighbor)
-
-    def deactivate(self, cube):
-        self.active.discard((cube))
-        for neighbor in self.get_neighbours(cube):
-            self.n_neighbors[neighbor] -= 1
-            if self.n_neighbors[neighbor] == 3:
-                self.candidates.add(neighbor)
-            elif self.n_neighbors[neighbor] == 2:
-                self.candidates.discard(neighbor)
-
-    def step(self):
-        old_active = self.active.copy()
-        old_n_neighbors = self.n_neighbors.copy()
-        old_candidates = self.candidates.copy()
-
-        for cube in old_active:
-            if old_n_neighbors[cube] not in [2, 3]:
-                self.deactivate(cube)
-        for cube in old_candidates - old_active:
-            self.activate(cube)
-
-    def get_activated_count(self):
-        return len(self.active)
-
-    @staticmethod
-    def get_neighbours(coords):
-        n_dim = len(coords)
-        for delta in product((-1, 0, 1), repeat=n_dim):
-            if delta != (0,) * n_dim:
-                yield tuple(coords[i] + delta[i] for i in range(n_dim))
-
-    def __str__(self):
-        coords_by_dim = zip(*self.active)
-        ranges = [(min(coords), max(coords) + 1) for coords in coords_by_dim]
-        lines = []
-        for coords in zip(*[range(*r) for r in ranges[2:]]):
-            lines.append(f"(X,X,{','.join([str(c) for c in coords])})")
-            for x in range(*ranges[0]):
-                lines.append(
-                    "".join(
-                        ACTIVE if (x, y, *coords) in self.active else INACTIVE
-                        for y in range(*ranges[1])
-                    )
+def pformat_pocket_dim(active):
+    coords_by_dim = zip(*active)
+    ranges = [(min(coords), max(coords) + 1) for coords in coords_by_dim]
+    lines = []
+    for coords in zip(*[range(*r) for r in ranges[2:]]):
+        lines.append(f"(X,X,{','.join([str(c) for c in coords])})")
+        for x in range(*ranges[0]):
+            lines.append(
+                "".join(
+                    "#" if (x, y, *coords) in active else "." for y in range(*ranges[1])
                 )
+            )
 
-            lines.append("")
-        return "\n".join(lines)
+        lines.append("")
+    return "\n".join(lines)
 
 
 def test_day17_part2():
