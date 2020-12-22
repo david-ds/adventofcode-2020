1aa3b132a5d175ca30e13c1e3ef3943087a0b8eb
Ayaz Badouraly <badouralix@users.noreply.github.com>
2020-12-22 19:29:52 +0100

    [template] add pytest function

--- tool/templates/template.py
+++ tool/templates/template.py
@@ -2,7 +2,6 @@ from tool.runners.python import SubmissionPy
 
 
 class {class_name}(SubmissionPy):
-
     def run(self, s):
         """
         :param s: input in string format
@@ -10,3 +9,16 @@ class {class_name}(SubmissionPy):
         """
         # Your code goes here
         pass
+
+
+def test_{author_name}():
+    """
+    Run `python -m pytest {submission_file}` to test the submission.
+    """
+    assert (
+        {class_name}().run(
+            """
+""".strip()
+        )
+        == None
+    )
